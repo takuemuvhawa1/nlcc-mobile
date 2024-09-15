7004eb8d601f7b4e5ff6ed08665564022456057e
Kelvin Mubatapasango <50402111+Mubatapasango@users.noreply.github.com>
2024-09-15 13:18:32 +0200

    15 Sept Outer

--- App.js
+++ App.js
@@ -6,11 +6,20 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Splash from './src/screens/Splash';
 import SignIn from './src/screens/SignIn';
 import Home from './src/screens/Home';
+import Services from './src/screens/Services';
+import Contributions from './src/screens/Contributions';
+import Events from './src/screens/Events';
+import More from './src/screens/More';
+import ForgotPassword from './src/screens/ForgotPassword';
+import Otp from './src/screens/Otp';
+import ResetPassword from './src/screens/ResetPassword';
+import About from './src/screens/About';
+import MyProfile from './src/screens/MyProfile';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [initialRouteName, setInitialRouteName] = React.useState('Splash');
+  const [initialRouteName, setInitialRouteName] = React.useState('About');
   
   // React.useEffect(() => {
   //   setTimeout(() => {
@@ -48,6 +57,15 @@ const App = () => {
             <Stack.Screen name="Splash" component={Splash} />
             <Stack.Screen name="SignIn" component={SignIn} />
             <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Services" component={Services} />
+            <Stack.Screen name="Contributions" component={Contributions} />
+            <Stack.Screen name="Events" component={Events} />
+            <Stack.Screen name="More" component={More} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+            <Stack.Screen name="Otp" component={Otp} />
+            <Stack.Screen name="ResetPassword" component={ResetPassword} />
+            <Stack.Screen name="About" component={About} />
+            <Stack.Screen name="MyProfile" component={MyProfile} />
            
           </Stack.Navigator>
         </>
